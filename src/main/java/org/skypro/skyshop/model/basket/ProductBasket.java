b7cd83568b7e575fce365c82532c8fce0afed953
1 <vasilijnenastev@gmail.com>
2025-02-27 20:16:07 +0400

    1.2

--- src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
+++ src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
@@ -1,18 +1,22 @@
 package org.skypro.skyshop.model.basket;
 
 
+import org.springframework.web.context.annotation.SessionScope;
+
 import java.util.Collections;
 import java.util.Map;
 import java.util.UUID;
 
+@SessionScope
 public class ProductBasket {
     private final Map<UUID, Integer> basket;
 
     public ProductBasket(Map<UUID, Integer> basket) {
+
         this.basket = basket;
     }
 
-    public Map<UUID, Integer> getBasket(){
+    public Map<UUID, Integer> getBasket() {
         return Collections.unmodifiableMap(basket);
     }
 
